8dbf2882b84388fcade4577a2fdab9470df59a91
Calvin Lau <calvinlau6@gmail.com>
2023-04-12 18:47:13 +0800

    Implement people model

---
+++ IngLabAssessment/Model/People.swift
@@ -0,0 +1,18 @@
+//
+//  People.swift
+//  IngLabAssessment
+//
+//  Created by Calvin Lau on 12/04/2023.
+//
+
+import Foundation
+
+struct People: Codable {
+    var avatar: String?
+    var name: String?
+    var phone: String?
+    var isActive: Bool?
+    var index: Int?
+    var emailAddress: String?
+    var description: String?
+}
