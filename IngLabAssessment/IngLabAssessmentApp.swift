eca4377d9fecba797f3a12d0a40f2c7042093583
Calvin Lau <calvinlau6@gmail.com>
2023-04-12 16:19:59 +0800

    Initial commit

---
+++ IngLabAssessment/IngLabAssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  IngLabAssessmentApp.swift
+//  IngLabAssessment
+//
+//  Created by Calvin Lau on 12/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct IngLabAssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
